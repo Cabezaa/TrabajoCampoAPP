937944bf72e1fbb3d1895a5eeef1f5072b5cb3c7
juampi <juanpabloorlando9498@gmail.com>
2017-09-19 00:21:53 -0300

    Fix(): fix para mongoose de los modelos + ruta documentos con mongo

--- GestionOrdenesBackend/src/routes/documentos.route.ts
+++ GestionOrdenesBackend/src/routes/documentos.route.ts
@@ -1,5 +1,7 @@
 import * as express from 'express';
 
+var Documento = require('../models/documento.model');
+
 class DocumentosRoute {
   public express;
   public router;
@@ -29,28 +31,72 @@ class DocumentosRoute {
     this.router = express.Router();
 
     this.router.get('/', (req, res) => {
+
+      Documento.find()
+      .exec( (err,documentos) => {
+          if(err){
+            return res.status(404).json({
+                title: 'Error al buscar documentos!',
+                error: err
+            });
+          }
+          else{
+            res.status(200).json({
+            message: 'Estas son los documentos!',
+            obj: documentos
+        });
+          }
+      })
+    });
+
+    this.router.get('/seed', (req, res) => {
+
+      for (let i = 0; i < this.documentosStub.length; i++) {
+          let docAux = new Documento({
+            idDocumento:  this.documentosStub[i].idDocumento,
+            descripcion:   this.documentosStub[i].descripcion,
+            linkArchivo:   this.documentosStub[i].linkArchivo,
+            nombreArchivo:   this.documentosStub[i].nombreArchivo
+          });
+
+          docAux.save().then(documentoNuevo => {
+            console.log('Documento guardado con exito!!');
+            console.log(documentoNuevo);
+          }, err => {
+            console.log('Error al guardar el documento!!');
+            console.log(err);
+          })
+      }
       res.status(200).json({
-        message: 'Estas son los documentos!',
-        obj: this.documentosStub
+        message: 'Se han guardado documentos en la base de datos!'
       });
     });
 
     this.router.get('/:idDocumento',(req,res)=>{
       let idDocumento = req.params.idDocumento;
       if(idDocumento != null){
-        for (let i = 0; i < this.documentosStub.length; i++) {
-            if(this.documentosStub[i].idDocumento===idDocumento){
-              res.status(200).json({
-                message: 'El documento seleccionado es el siguiente!',
-                obj: this.documentosStub[i]
+
+        Documento.findOne({'idDocumento': idDocumento})
+        .exec( (err, documento) => {
+            if(err){
+              return res.status(404).json({
+                  title: 'Error al buscar el documento!',
+                  error: err
               });
             }
-        }
+            else{
+              res.status(200).json({
+              message: 'Este es el documento buscado!',
+              obj: documento
+          });
+            }
+        })
+
       }
       else{
         return res.status(400).json({
           title: 'Error',
-          error: 'El idDocumento brindado no es correcto!'
+          error: 'No existe un documento con tal id del docmento!!'
         });
       }
     });
