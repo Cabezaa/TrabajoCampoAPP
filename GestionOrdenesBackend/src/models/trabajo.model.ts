d8cb2d0869556b5cbc12e35474eb8c5d75b78705
juampi <juanpabloorlando9498@gmail.com>
2017-09-19 01:11:37 -0300

    Change(): cambio en modelo de Trabajo

--- GestionOrdenesBackend/src/models/trabajo.model.ts
+++ GestionOrdenesBackend/src/models/trabajo.model.ts
@@ -5,14 +5,16 @@ var Schema = mongoose.Schema;
 
 var OrdenServicio = require('./orden.model');
 var TipoTrabajo = require('./tipoTrabajo.model');
+var Pieza = require('./pieza.model');
 
 var trabajo_schema = new Schema({
-  numeroTrabajo: { type: String, required: true, unique: true },
+  numTrabajo: { type: String, required: true, unique: true },
   fechaRealizacion: Date,
   evaluacion: String,
   observaciones: String,
   ordenServicio: {type: Schema.ObjectId, ref: 'OrdenServicio',required: true},
   tipoTrabajo: {type: Schema.ObjectId, ref: 'TipoTrabajo', required: true},
+  pieza: {type: Schema.ObjectId, ref: 'Pieza', required: true},
   cuilSupervisor: String
 });
 
