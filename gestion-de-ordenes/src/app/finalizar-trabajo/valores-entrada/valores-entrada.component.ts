fa3ebe56753982cf01c1ac45baef07f87cf8afda
Esteban Ruiz de Galarreta <cabeza180@gmail.com>
2017-09-19 00:24:35 -0300

    Agregado sweetalert para confirmar los resultados finales..

--- gestion-de-ordenes/src/app/finalizar-trabajo/valores-entrada/valores-entrada.component.ts
+++ gestion-de-ordenes/src/app/finalizar-trabajo/valores-entrada/valores-entrada.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit,Input, Output, EventEmitter } from '@angular/core';
 import { TipoParametroService } from '../../servicios/tipoParametro.service';
 
+import {default as swal} from 'sweetalert2';
+
 @Component({
   selector: 'app-valores-entrada',
   templateUrl: './valores-entrada.component.html',
@@ -49,6 +51,42 @@ export class ValoresEntradaComponent implements OnInit {
     // ACA LO GUARDAMOS
 
     this.saveSuccess = true;
+
+    let texto = '';
+    for (let i = 0; i < resultadosIngresados.length; i++) {
+      texto = texto + '<b>' + this.parametros[i].nombreAtributo + '</b>: ' + resultadosIngresados[i].valor + "</br>";
+    }
+
+    swal({
+      title: 'Estas seguro de estos resultados?',
+      // text: 'Valores ingresados: ` '+texto,
+      html: '<p>Valores Ingresados: </p> </br>'+texto,
+      type: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, confirmar datos!',
+      cancelButtonText: 'No, cancelar!',
+      confirmButtonClass: 'btn btn-success',
+      cancelButtonClass: 'btn btn-danger',
+      buttonsStyling: false
+    }).then(function () {
+      swal(
+        'Enviados!',
+        'Sus resultados fueron registrados correctamente',
+        'success'
+      )
+    }, function (dismiss) {
+      // dismiss can be 'cancel', 'overlay',
+      // 'close', and 'timer'
+      if (dismiss === 'cancel') {
+        swal(
+          'Cancelado',
+          'Sus datos no fueron enviados',
+          'error'
+        )
+      }
+    });
   }
   cancelar(){
     this.model = {};
