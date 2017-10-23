2cd1dddc9c685f1231c54ffbd0a5f29d37d22302
juampi <juanpabloorlando9498@gmail.com>
2017-10-23 15:40:24 -0300

    Add(): modelo Personal

---
+++ GestionOrdenesBackend/src/models/personal.model.ts
@@ -0,0 +1,19 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+var personal_schema = new Schema({
+  cuil: { type: String, required: true, unique: true },
+  nombre: String,
+  apellido: String,
+  telefonos: [String],
+  direccion: String,
+  puesto: String
+});
+
+// El esquema solo no sirve. Luego, creamos el modelo
+var Personal = mongoose.model('Personal',personal_schema);
+
+module.exports = Personal;
